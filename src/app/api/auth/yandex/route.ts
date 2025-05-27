c501e362cd2b46deea6d2106fe5f0e3d4f270102
Urakova Anna <Urakova_aa@directum.ru>
2025-05-27 15:37:13 +0400

    Исправлен README + env

--- src/app/api/auth/yandex/route.ts
+++ src/app/api/auth/yandex/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: Request) {
+  const redirectUri = new URL(process.env.YANDEX_REDIRECT_URI!, new URL(request.url).origin).href;
   const authUrl = new URL('https://oauth.yandex.ru/authorize');
   authUrl.searchParams.set('response_type', 'code');
   authUrl.searchParams.set('client_id', process.env.YANDEX_CLIENT_ID!);
-  authUrl.searchParams.set('redirect_uri', process.env.YANDEX_REDIRECT_URI!);
+  authUrl.searchParams.set('redirect_uri', redirectUri);
 
   return NextResponse.redirect(authUrl.toString());
 }
